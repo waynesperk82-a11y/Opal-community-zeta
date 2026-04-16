e188dab143c42743a899a21247da39a7dcf60266
Youngspud <waynesperk82@gmail.com>
2026-04-16 06:36:59 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,4 +1,2 @@
 export * from "./generated/api";
-export * from "./generated/api.schemas";
-export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
-export type { AuthTokenGetter } from "./custom-fetch";
+export * from "./generated/types";
