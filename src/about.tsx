03e502b91d932693df53838908ee3bbe258215c0
Youngspud <waynesperk82@gmail.com>
2026-04-21 07:55:55 +0300

    Refactor imports in about.tsx

--- src/about.tsx
+++ src/about.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/layout"
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge";
 import { BrainCircuit, CheckCircle2, Clock, MessageSquare, ShieldCheck, Users } from "lucide-react";
 
