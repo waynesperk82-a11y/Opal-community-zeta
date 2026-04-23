129175c970307426208fd4a1b026122a76453c4c
Youngspud <waynesperk82@gmail.com>
2026-04-23 23:09:42 +0300

    Rename AskQuestion to Ask and add Feed route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
-import AskQuestion from "./pages/AskQuestion";
+import Ask from "./pages/Ask";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/ask" component={AskQuestion} />
+      <Route path="/ask" component={Ask} />
+      <Route path="/feed" component={Feed} />
     </Switch>
   );
 }
