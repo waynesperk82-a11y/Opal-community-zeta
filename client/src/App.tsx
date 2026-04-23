ed506e535d04cfa08cc4042f17f084549f630c67
Youngspud <waynesperk82@gmail.com>
2026-04-23 22:50:48 +0300

    Add AskQuestion route to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
+import AskQuestion from "./pages/AskQuestion";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/ask" component={AskQuestion} />
     </Switch>
   );
 }
