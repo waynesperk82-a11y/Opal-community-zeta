cf7e29af495e327c6646e926bcd7835f946eeb8f
Youngspud <waynesperk82@gmail.com>
2026-04-23 23:14:09 +0300

    Remove Ask route from App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
-import Ask from "./pages/Ask";
 import Feed from "./pages/Feed";
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/ask" component={Ask} />
       <Route path="/feed" component={Feed} />
     </Switch>
   );
