69d6872de859da29f8a4156edeb46d0c8100b667
Youngspud <waynesperk82@gmail.com>
2026-04-24 08:04:19 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css"; // ← THIS WAS MISSING
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
