28cb539f46e5063e61bca8bfe66b0fb7b3a9c0e7
Youngspud <waynesperk82@gmail.com>
2026-04-24 08:10:23 +0300

    Refactor main.tsx by removing BrowserRouter
    Removed BrowserRouter wrapper and ensured index.css is imported.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // ← THIS WAS MISSING
+import "./index.css"; // make sure this exists
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
