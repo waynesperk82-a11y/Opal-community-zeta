cf23fbab72c35e812504855a49463e60665f88bc
Youngspud <waynesperk82@gmail.com>
2026-04-24 07:22:46 +0300

    Create Not Found.tsx

---
+++ client/src/pages/Not Found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white bg-red-900">
+      404 - Page Not Found
+    </div>
+  );
+}
